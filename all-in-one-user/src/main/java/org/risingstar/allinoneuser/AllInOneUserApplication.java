eb9011fee3b09e6370507e971b2a9f0ed66c3d3b
CHANGSHAQI <1073505842@qq.com>
2019-12-25 14:28:59 +0800

    12.25 项目整体结构搭建

---
+++ all-in-one-user/src/main/java/org/risingstar/allinoneuser/AllInOneUserApplication.java
@@ -0,0 +1,13 @@
+package org.risingstar.allinoneuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInOneUserApplication.class, args);
+    }
+
+}
---
+++ all-in-one-user/src/test/java/org/risingstar/allinoneuser/AllInOneUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.risingstar.allinoneuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllInOneUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
